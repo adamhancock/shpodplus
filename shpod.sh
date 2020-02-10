63e3efc507795a262f15d8c54aacf6b8abc85909
adammhancock@gmail.com <adammhancock@gmail.com>
2020-02-10 22:57:44 +0000

    added function site

--- shpod.sh
+++ shpod.sh
@@ -2,14 +2,15 @@
 # For more information about shpod, check it out on GitHub:
 # https://github.com/jpetazzo/shpod
 
-YAML=https://cdn.a9k.io/get/shpod.yaml
-echo "namespace?"; read namespace
+echo "Namespace?"
+read namespace
+YAML=https://shpod.a9k.io/api/run?n=$namespace
 echo "Applying YAML: $YAML..."
 kubectl apply -f $YAML -n $namespace
 echo "Waiting for pod to be ready..."
 kubectl wait --namespace=$namespace --for condition=Ready pod/shpod
 echo "Attaching to the pod..."
-kubectl -n namespace attach --namespace=shpod -ti shpod </dev/tty
+kubectl -n namespace attach --namespace=$namespace -ti shpod </dev/tty
 echo "Deleting pod..."
 echo "
 Note: it's OK to press Ctrl-C if this takes too long and you're impatient.
